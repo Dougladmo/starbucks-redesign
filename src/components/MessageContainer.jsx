4ea8f6f02079371e1ef34e653fca21de702719e8
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2023-11-28 00:40:02 -0300

    Atualizar o MessageContainer.jsx

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -4,7 +4,7 @@ import background from '../assets/background.jpg'
 
 const MessageContainer = () => {
   return (
-    <div style={{backgroundImage: `url(${background})`}} className='w-11/12 md:w-full m-auto h-56 mb-12 bg-center bg-no-repeat bg-cover flex flex-col items-center justify-center gap-8'>
+    <div style={{backgroundImage: `url(${background})`}} className='w-11/12 md:w-full m-auto h-56 mb-12 bg-center bg-no-repeat bg-cover flex flex-col items-center justify-center gap-5'>
         <p className='px-1 text-xl md:text-base max-w-3/4 md:pt-8 text-center'>“A cada xícara, a cada conversa, com cada comunidade – Nós nutrimos infinitas possibilidades de conexão humana.”</p>
         <LightButton text='Saiba mais' />
     </div>
